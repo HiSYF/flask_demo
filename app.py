738d69a1058fa404ef71f87ae8bc2267f2ed4ced
sunyf98 <198067067@qq.com>
2021-11-10 13:59:51 +0800

    Add index page

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask import url_for
 import douyin
 import webbrowser
 
 app = Flask(__name__)
 @app.route('/')
-def hello():
-    return 'Welcome to My Watchlist!'
+def index():
+    name = 'Grey Li'
+    movies = [
+        {'title': 'My Neighbor Totoro', 'year': '1988'},
+        {'title': 'Dead Poets Society', 'year': '1989'},
+        {'title': 'A Perfect World', 'year': '1993'},
+        {'title': 'Leon', 'year': '1994'},
+        {'title': 'Mahjong', 'year': '1996'},
+        {'title': 'Swallowtail Butterfly', 'year': '1996'},
+        {'title': 'King of Comedy', 'year': '1999'},
+        {'title': 'Devils on the Doorstep', 'year': '1999'},
+        {'title': 'WALL-E', 'year': '2008'},
+        {'title': 'The Pork of Music', 'year': '2012'},
+    ]
+    return render_template('index.html', name=name, movies=movies)
 @app.route('/user/<name>')
 def user_page(name):
     return f'User: {name}'
